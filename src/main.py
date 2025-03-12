ceecae6b8751eea82dd93f1802f59e41ab90ed47
Curtis-P <curtis.paff@gmail.com>
2025-03-11 20:08:08 -0400

    generate pages recursively func implemented

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def main():
     
     clear_dir("public")
     copy_dir("static", "public")
-    generate_page('content/index.md', 'template.html', 'public/index.html')
+    generate_pages_recursively('content', 'template.html', 'public')
 
 
 def clear_dir(dest_dir):
@@ -48,12 +48,28 @@ def generate_page(from_path, template_path, dest_path):
         template_content = template_fs.read()
     title = extract_title(new_content)
     new_content = markdown_to_html_node(new_content).to_html()
-    print(f"***{new_content}***")
+    
     templated_content = template_content.replace("{{ Title }}", title)   
     templated_content = templated_content.replace("{{ Content }}", new_content)   
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w') as dest_fs:
          dest_fs.write(templated_content)
+
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+    print(f"***    Generating page from {dir_path_content} to {dest_dir_path} using {template_path}    ***")
+    file_structure = os.listdir(dir_path_content)
+    for fs_object in file_structure:
+        current_path = os.path.join(dir_path_content, fs_object)
+        if os.path.isfile(current_path) and current_path.endswith(".md"):
+            dest_file_path = os.path.join(dest_dir_path, fs_object.replace('.md','.html'))
+            generate_page(current_path,template_path,dest_file_path)
+        elif os.path.isdir(current_path):
+            sub_dest_dir_path = os.path.join(dest_dir_path, fs_object)
+            os.makedirs(sub_dest_dir_path, exist_ok=True)
+            generate_pages_recursively(current_path, template_path, sub_dest_dir_path)
+    
+    
+    
 
 if __name__ == "__main__":
     main()
