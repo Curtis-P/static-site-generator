6a6cc2beeec5b9f063cf1c3be1477d369d94b63e
Curtis-P <curtis.paff@gmail.com>
2025-04-15 07:42:25 -0400

    added dynamic basepath for remote hosting and build script

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 
-
+import sys
 import shutil
 import os
 from markdown_to_html import markdown_to_html_node
 
 def main():
-    
+    if len(sys.argv > 1):
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
     clear_dir("public")
     copy_dir("static", "public")
-    generate_pages_recursively('content', 'template.html', 'public')
+    generate_pages_recursively('content', 'template.html', 'public', basepath)
 
 
 def clear_dir(dest_dir):
@@ -40,7 +43,7 @@ def extract_title(markdown):
         
     raise Exception("No H1 Title in document")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as content_fs:
         new_content = content_fs.read()
@@ -51,22 +54,25 @@ def generate_page(from_path, template_path, dest_path):
     
     templated_content = template_content.replace("{{ Title }}", title)   
     templated_content = templated_content.replace("{{ Content }}", new_content)   
+    templated_content = templated_content.replace('href="/', f'href="{basepath}')   
+    templated_content = templated_content.replace('src="/', f'src="{basepath}')   
+
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w') as dest_fs:
          dest_fs.write(templated_content)
 
-def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path, basepath):
     print(f"***    Generating page from {dir_path_content} to {dest_dir_path} using {template_path}    ***")
     file_structure = os.listdir(dir_path_content)
     for fs_object in file_structure:
         current_path = os.path.join(dir_path_content, fs_object)
         if os.path.isfile(current_path) and current_path.endswith(".md"):
             dest_file_path = os.path.join(dest_dir_path, fs_object.replace('.md','.html'))
-            generate_page(current_path,template_path,dest_file_path)
+            generate_page(current_path,template_path,dest_file_path, basepath)
         elif os.path.isdir(current_path):
             sub_dest_dir_path = os.path.join(dest_dir_path, fs_object)
             os.makedirs(sub_dest_dir_path, exist_ok=True)
-            generate_pages_recursively(current_path, template_path, sub_dest_dir_path)
+            generate_pages_recursively(current_path, template_path, sub_dest_dir_path, basepath)
     
     
     
